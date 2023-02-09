a61d867286a8553919a002eb0c622edbde77652d
MoonYoungMin <ansdudals8@naver.com>
2023-02-10 01:05:48 +0900

    remove .git files

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/'; // 중복되는 부분을 굳이 텍스트로 치지 않아도, 이미지들을 가져올 수 있는거임
+export const API_KEY = 'ca1ccf3d709feb602e1ec208241fc6bd';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
